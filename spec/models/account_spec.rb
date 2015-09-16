e9765e1af4475ca385fb2937fc9a969cb6308215
Kirill Kayumov <kirill.kayumov@flatstack.com>
2015-09-16 20:54:43 +0300

    Add module tests for models and services

---
+++ spec/models/account_spec.rb
@@ -0,0 +1,8 @@
+require "rails_helper"
+
+describe Account do
+  it { is_expected.to belong_to :user }
+  it { is_expected.to validate_presence_of :provider }
+  it { is_expected.to validate_presence_of :uid }
+  it { is_expected.to validate_presence_of :user }
+end
