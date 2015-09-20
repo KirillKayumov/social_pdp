bbb66b7cc2dd1bf345c1d6cb2cd837c584958abc
Kirill Kayumov <kirill.kayumov@flatstack.com>
2015-09-20 10:25:28 +0300

    Add ability to update profile without password

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -9,7 +9,29 @@ class RegistrationsController < Devise::RegistrationsController
 
   private
 
+  def update_resource(resource, params)
+    if password_present?(params)
+      resource.update_with_password(params)
+    else
+      resource.update_without_password(params_without_password(params))
+    end
+  end
+
   def after_update_path_for(_resource)
     edit_user_registration_path
+  end
+
+  def password_present?(params)
+    params[:password].present? ||
+      params[:password_confirmation].present? ||
+      params[:current_password].present?
+  end
+
+  def params_without_password(params)
+    params.except(
+      :password,
+      :password_confirmation,
+      :current_password
+    )
   end
 end
