b57a16b4c0c3fad890877ffcf4663ef9d54b3d75
Kirill Kayumov <kirill.kayumov@flatstack.com>
2015-09-20 16:31:35 +0300

    Update default email address

--- config/application.rb
+++ config/application.rb
@@ -29,7 +29,7 @@ module SocialPDP
     config.slim_options = {}
 
     # Default e-mail address which will be shown in the "from" devise emails, initializers/devise.rb,
-    config.noreply = "noreply@fs-rails-base.herokuapp.com"
+    config.noreply = "noreply@social-pdp.herokuapp.com"
 
     # Default host for action mailer, initializers/mailer.rb
     config.host = "localhost:5000"
